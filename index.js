f6963afc52968dfd29ceb6865c52f5253c1539da
Sarah Azad <aras.azad3@gmail.com>
2024-03-08 18:18:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" },
+  //...
+];
+
+// function superbowlWin(array) {
+//   const foundWin = array.find(function (object) {
+//     if (object.result === "W") {
+//       return object.year;
+//     }
+//   });
+//   return foundWin.year;
+// }
+
+function superbowlWin(array) {
+  const winningGame = array.find((game) => game.result === "W");
+  return winningGame ? winningGame.year : undefined;
+}
+
+console.log(superbowlWin(record));
